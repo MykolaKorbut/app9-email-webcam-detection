15ba00ee73d744b23d1eb815bf035aa333b1d8e2
mikolakorbut <nikolay.korbut@gmail.com>
2024-09-14 10:38:46 +0300

    Create image with object from camera

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import cv2
 import time
 from emailing import send_email
+import glob
 
 video = cv2.VideoCapture(1)
 time.sleep(1)
 
 first_frame = None
 status_list = []
-
+count = 0
 while True:
     status = 0
     check, frame = video.read()
@@ -25,13 +26,18 @@ while True:
     contours, check = cv2.findContours(dil_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     for contour in contours:
-        if cv2.contourArea(contour) < 10000:
+        if cv2.contourArea(contour) < 5000:
             continue
         x, y, w, h = cv2.boundingRect(contour)
         rectangle = cv2.rectangle(frame, (x, y), (x + w, y + h),
                                   (0, 255, 0), 3)
         if rectangle.any():
             status = 1
+            cv2.imwrite(f"images/{count}.png", frame)
+            count = count + 1
+            all_images = glob.glob("images/*.png")
+            index = int(len(all_images) / 2)
+            image_with_object = all_images[index]
 
     status_list.append(status)
     status_list = status_list[-2:]
